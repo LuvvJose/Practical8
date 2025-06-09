4699d631c48bc7652670ad5c578374f99deb6b36
Jose <joseedusolsona@gmail.com>
2025-06-09 22:29:43 +0200

    Add project files.

---
+++ Models/Ellipse.cs
@@ -0,0 +1,42 @@
+﻿using Practical8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practical8.Models
+{
+    public class Ellipse : Shape
+    {
+        public int Cx { get; set; }
+        public int Cy { get; set; }
+        public int Rx { get; set; }  
+        public int Ry { get; set; }  
+
+        public Ellipse() { }
+
+        public Ellipse(string description, string fill, string strokeColour, double strokeWidth,
+                       int cx, int cy, int rx, int ry)
+            : base(description, fill, strokeColour, strokeWidth)
+        {
+            Cx = cx;
+            Cy = cy;
+            Rx = rx;
+            Ry = ry;
+        }
+
+        public override double getArea()
+        {
+            return Math.PI * Rx * Ry;
+        }
+
+        public override string getSVG()
+        {
+            return $"<svg width='{SvgWidth}' height='{SvgHeight}'>" +
+                   $"<ellipse cx='{Cx}' cy='{Cy}' rx='{Rx}' ry='{Ry}' " +
+                   $"fill='{Fill}' stroke='{StrokeColour}' stroke-width='{StrokeWidth}' />" +
+                   $"</svg>";
+        }
+    }
+
+}
---
+++ Models/Triangle.cs
@@ -0,0 +1,49 @@
+﻿using Practical8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practical8.Models
+{
+    public class Triangle : Shape
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int BaseLength { get; set; }
+        public int Height { get; set; }
+
+        public Triangle() { }
+
+        public Triangle(string description, string fill, string strokeColour, double strokeWidth,
+                        int x, int y, int baseLength, int height)
+            : base(description, fill, strokeColour, strokeWidth)
+        {
+            X = x;
+            Y = y;
+            BaseLength = baseLength;
+            Height = height;
+        }
+
+        public override double getArea()
+        {
+            return 0.5 * BaseLength * Height;
+        }
+
+        public override string getSVG()
+        {
+            int x1 = X;
+            int y1 = Y;
+            int x2 = X + BaseLength;
+            int y2 = Y;
+            int x3 = X + (BaseLength / 2);
+            int y3 = Y - Height;
+
+            return $"<svg width='{SvgWidth}' height='{SvgHeight}'>" +
+                   $"<polygon points='{x1},{y1} {x2},{y2} {x3},{y3}' " +
+                   $"fill='{Fill}' stroke='{StrokeColour}' stroke-width='{StrokeWidth}' />" +
+                   $"</svg>";
+        }
+    }
+
+}
